3e630da1020f8a7b88719add4ab263622fb50ce0
manu-cj <carenjotje@gmail.com>
2025-07-06 18:04:32 +0200

    Fix: login now fonctionnal

---
+++ src/main/java/com/manu/template/security/UserDetailsServiceConfig.java
@@ -0,0 +1,26 @@
+package com.manu.template.security;
+
+import com.manu.template.model.User;
+import com.manu.template.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class UserDetailsServiceConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(user.getUsername())
+                    .password(user.getPassword())
+                    .authorities(user.getRoles().stream().map(r -> "ROLE_" + r).toArray(String[]::new))
+                    .build();
+        };
+    }
+}
